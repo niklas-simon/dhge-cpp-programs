9cf6dfa0125527b23e5fc506604dc865be870db4
Niklas <niklaspein12@gmail.com>
2023-07-11 09:08:24 +0200

    wordle-solver

--- wordle-solver/program.c
+++ wordle-solver/program.c
@@ -2,100 +2,231 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-#define LENGTH 10657
 
-char words[LENGTH][5];
+char **last;
+char **misplaced;
+char present[5];
+char correct[5];
+char absent[26];
 
-char chars[26] = "EARIOTNSLCUDPMHGBFYWKVXZJQ";
+typedef struct LCNT {
+    char chr;
+    int cnt;
+} LCNT;
 
-char *last;
-unsigned int correct = 0x0;
-unsigned int present = 0x0;
+typedef struct WSCR {
+    const char *word;
+    int score;
+} WSCR;
 
-bool valid(char *guess) {
-    for (int i = 0; i < 5; i++) {
-        if (!guess[i]) {
-            return true;
+int lcnt_sort(const void *a, const void *b) {
+    return ((LCNT *)b)->cnt - ((LCNT *)a)->cnt;
+}
+
+int wscr_sort(const void *a, const void *b) {
+    return ((WSCR *)b)->score - ((WSCR *)a)->score;
+}
+
+int main(int argc, char **argv) {
+    if (argc % 2 != 1) {
+        fprintf(stderr, "usage: %s [guess value]*\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // read wordlist
+    FILE *f = fopen("list.txt", "r");
+    int used = 1024;
+    char **wordlist = malloc(used * sizeof(char *));
+    int length;
+    char c = getc(f);
+    for (length = 0;; length++) {
+        if (length >= used) {
+            used *= 2;
+            wordlist = realloc(wordlist, used * sizeof(char *));
+            if (!wordlist) {
+                fprintf(stderr, "%s: reallocation error\n", argv[0]);
+                exit(EXIT_FAILURE);
+            }
         }
-        if ((correct & (0x1 << i)) && guess[i] != last[i]) {
-            return false;
+        wordlist[length] = malloc(5 * sizeof(char));
+        if (!wordlist[length]) {
+            fprintf(stderr, "%s: allocation error\n", argv[0]);
+            exit(EXIT_FAILURE);
         }
-        if (present & (0x1 << i)) {
+        for (int i = 0; i < 5 && c != EOF && c >= 'a' && c <= 'z'; i++) {
+            wordlist[length][i] = c;
+            c = getc(f);
+        }
+        for (;c != EOF && (c < 'a' || c > 'z'); c = getc(f)) {}
+        if (c == EOF) {
+            break;
+        }
+    }
+    fclose(f);
+
+    // letter count
+    LCNT lcnt[26];
+    for (int i = 0; i < 26; i++) {
+        lcnt[i] = (LCNT){'a' + i, 0};
+    }
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < 5; j++) {
+            lcnt[wordlist[i][j] - 'a'].cnt++;
+        }
+    }
+    qsort(lcnt, 26, sizeof(LCNT), lcnt_sort);
+
+    // word scores
+    WSCR wscr[length];
+    for (int i = 0; i < length; i++) {
+        int sum = 0;
+        for (int j = 0; j < 5; j++) {
             bool found = false;
-            for (int j = 0; j < 5; j++) {
-                if (last[i] == guess[j]) {
+            for (int k = 0; k < j; k++) {
+                if (wordlist[i][j] == wordlist[i][k]) {
                     found = true;
                     break;
                 }
             }
-            if (!found) {
-                return false;
-            }
+            if (found) continue;
+            sum += lcnt[wordlist[i][j] - 'a'].cnt;
         }
+        wscr[i] = (WSCR){wordlist[i], sum};
     }
-    // optimization needed
-    bool found = false;
-    for (int i = 0; i < LENGTH; i++) {
-        if (strcmp(words[i], guess) == 0) {
-            found = true;
-            break;
-        }
-    }
-    return found;
-}
+    qsort(wscr, length, sizeof(WSCR), wscr_sort);
 
-bool find_next(char *guess, int index) {
-    if (index >= 5) {
-        return true;
+    // generate words array
+    const char *words[length];
+    for (int i = 0; i < length; i++) {
+        words[i] = wscr[i].word;
     }
-    for (int i = 0; i < 26; i++) {
-        guess[index] = chars[i];
-        printf("%s\n", guess);
-        if (valid(guess) && find_next(guess, index + 1)) {
-            return true;
-        }
-        guess[index] = '\0';
-    }
-    return false;
-}
 
-int main(int argc, char **argv) {
-    if (argc != 3 && argc != 1) {
-        fprintf(stderr, "usage: %s [guess value]\n", argv[0]);
+    int l_last = (argc - 1) / 2;
+    last = malloc(l_last * sizeof(char *));
+    misplaced = malloc(l_last * sizeof(char *));
+    if (!last) {
+        fprintf(stderr, "%s: allocation error\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    if (argc == 3) {
-        if (strlen(argv[1]) != 5 || strlen(argv[2]) != 5) {
+
+    for (int i = 0; i < 5; i++) {
+        present[i] = '\0';
+        correct[i] = '\0';
+        absent[i] = '\0';
+    }
+    for (int i = 5; i < 26; i++) {
+        absent[i] = '\0';
+    }
+
+    for (int i = 0; i < l_last; i++) {
+        if (strlen(argv[i * 2 + 1]) != 5 || strlen(argv[i * 2 + 2]) != 5) {
             fprintf(stderr, "%s: guess and value must be of length 5\n", argv[0]);
             exit(EXIT_FAILURE);
         }
-        last = argv[1];
-        for (int i = 0; i < 5; i++) {
-            switch (argv[2][i]) {
+        last[i] = argv[i * 2 + 1];
+
+        misplaced[i] = malloc(5 * sizeof(char));
+        if (!misplaced[i]) {
+            fprintf(stderr, "%s: allocation error\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+
+        for (int j = 0; j < 5; j++) {
+            int k;
+            misplaced[i][j] = '\0';
+            switch (argv[i * 2 + 2][j]) {
+                case '0':
+                    for (k = 0; k < 26 && absent[k] && argv[i * 2 + 1][j] != absent[k]; k++) {}
+                    if (k < 26 && !absent[k]) {
+                        absent[k] = argv[i * 2 + 1][j];
+                    }
+                    break;
                 case '1':
-                    present |= 0x1 << i;
+                    for (k = 0; k < 5 && present[k] && argv[i * 2 + 1][j] != present[k]; k++) {}
+                    if (k < 26 && !present[k]) {
+                        present[k] = argv[i * 2 + 1][j];
+                    }
+                    misplaced[i][j] = argv[i * 2 + 1][j];
+                    break;
                 case '2':
-                    correct |= 0x1 << i;
+                    correct[j] = argv[i * 2 + 1][j];
+                    break;
             }
         }
     }
     
-    FILE *wordlist = fopen("words.txt", "r");
-    for (int i = 0; i < LENGTH; i++) {
-        if (!fgets(words[i], 5, wordlist)) {
-            fprintf(stderr, "%s: error reading wordlist\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
-    }
-    fclose(wordlist);
+    int i;
+    for (i = 0; i < length; i++) {
+        const char *guess = words[i];
+        bool found = true;
 
-    
-    char next[5] = "\0\0\0\0\0";
-    if (find_next(next, 0)) {
-        printf("%s\n", next);
-        return EXIT_SUCCESS;
+        // is correct
+        for (int j = 0; j < 5; j++) {
+            if (correct[j] && guess[j] != correct[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (!found) {
+            continue;
+        }
+
+        // is absent
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k < 26; k++) {
+                if (!absent[k]) break;
+                if (guess[j] == absent[k]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (!found) {
+                break;
+            }
+        }
+        if (!found) {
+            continue;
+        }
+
+        // is present
+        for (int j = 0; j < 5; j++) {
+            if (!present[j]) break;
+            bool is_present = false;
+            for (int k = 0; k < 5; k++) {
+                if (present[j] == guess[k]) {
+                    is_present = true;
+                    break;
+                }
+            }
+            if (!is_present) {
+                found = false;
+                break;
+            }
+        }
+        if (!found) {
+            continue;
+        }
+
+        // misplaced placed the same
+        for (int j = 0; j < l_last; j++) {
+            for (int k = 0; k < 5; k++) {
+                if (misplaced[j][k] == guess[k]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (!found) break;
+        }
+        if (!found) {
+            continue;
+        }
+        if (found) break;
+    }
+    if (i < length) {
+        printf("%s\n", words[i]);
+        exit(EXIT_SUCCESS);
     } else {
         fprintf(stderr, "%s: no solution found\n", argv[0]);
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 }
